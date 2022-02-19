3c41befa5c16ab5587a63c9d3626399e630231ef
ZenkinIlya <56563000+ZenkinIlya@users.noreply.github.com>
2022-02-19 19:45:07 +0300

    update action bar

--- app/src/main/java/com/example/githubapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.githubapp.R
 import com.example.githubapp.databinding.ActivityMainBinding
@@ -16,10 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Set ActionBar
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        //Builder with ids fragments which do not have upButton
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.loginFragment,
+            R.id.listRepositoryFragment
+        ).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+//        navController.addOnDestinationChangedListener({ controller, destination, arguments ->
+//            destination.id ==
+//        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
